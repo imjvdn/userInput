66e781bdd8a5049189f3a85ff0990913255c6c84
Jadan morrow <jadanmorrow@outlook.com>
2020-09-21 12:14:41 -0600

    daily

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace userInput
            string name =  Console.ReadLine();
             Console.Write("Enter your age: ");
            string age = Console.ReadLine();
-            Console.Write("Enter your favorite food: ");
+            Console.Write("Enter your favorite drink: ");
            string color = Console.ReadLine();
 
             Console.WriteLine("Hello " + name + ", you are " + age + " and your favorite color is " + color + ".");
